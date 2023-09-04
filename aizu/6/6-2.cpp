a283f4265bae2db7db0f8b62acf06f42c4849c71
ohno129 <matoi13129@gmail.com>
2023-09-04 17:52:50 +0900

    6

---
+++ aizu/6/6-2.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+
+int main()
+{
+    double a,b;
+    char plus='+',equal='=';
+
+    a=0.3,b=1.7;
+
+    printf("%f %c %f %c %f\n",a,plus,b,equal,a+b);
+
+    return 0;
+}
---
+++ aizu/6/6-5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double a,b;
+    char plus='+',equal='=';
+
+    a=0.3,b=1.7;
+
+    cout<<a<<" "<<plus<<" "<<b<<" "<<equal<<" "<<a+b<<endl;
+
+    return 0;
+}
